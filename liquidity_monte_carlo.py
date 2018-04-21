aae9e5bdc98974f4fd061ea3115a8b3d2e723570
Neil <neil.ronald.irvine@gmail.com>
2018-04-21 12:09:40 -0400

    Setup normal Gaussian distribution.

--- liquidity_monte_carlo.py
+++ liquidity_monte_carlo.py
@@ -2,6 +2,12 @@ import random
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+import scipy.stats as st
 
-dist_one = random.randint(1, 20)
-dist_two = random.randint(1, 40)
+gaus_dist = np.random.randn(15000)
+plt.hist(gaus_dist, bins=40)
+plt.title("Money flow distribution")
+plt.xlabel("Money flow")
+plt.ylabel("Frequency")
+
+plt.show()
