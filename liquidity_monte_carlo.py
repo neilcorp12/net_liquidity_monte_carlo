869111e92485cb01709229b483b655d9fe4143eb
Neil <neil.ronald.irvine@gmail.com>
2018-04-25 23:55:17 -0400

    Use initial cash balance.

--- liquidity_monte_carlo.py
+++ liquidity_monte_carlo.py
@@ -4,11 +4,15 @@ import matplotlib
 import matplotlib.pyplot as plt
 import scipy.stats as st
 
+initial_cash_balance = 10000000
 translation = 2
 gaus_dist = np.random.randn(15000)
-plt.hist(gaus_dist + translation, bins=40)
-plt.title("Money flow distribution")
-plt.xlabel("Money flow")
-plt.ylabel("Frequency")
 
-plt.show()
+print()
+
+# plt.hist(gaus_dist + translation, bins=40)
+# plt.title("Money flow distribution")
+# plt.xlabel("Money flow")
+# plt.ylabel("Frequency")
+#
+# plt.show()
